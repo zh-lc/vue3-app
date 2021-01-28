75ae2ce72bffca29e24366f0a0ecc66ff9a67a1e
LC2YYYY <lc2yyyy@163>
2021-01-28 09:52:02 +0800

    方便调试

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,8 +1,21 @@
-import MNav from '../components/MNav'
+import { useStore } from 'vuex'
+import { Button, Cell, Switch } from 'vant'
 
-export default () => (
-  <>
-    <div>index</div>
-    <MNav />
-  </>
-)
+export default {
+  setup() {
+    const store = useStore()
+
+    const SwitchSlots = {
+      'right-icon': () => (
+        <Switch size="24" />
+      ),
+    };
+
+    return () => (
+      <>
+        <Cell title="默认地址" v-slots={SwitchSlots}></Cell>
+        <Switch size="24" />
+      </>
+    )
+  }
+}
